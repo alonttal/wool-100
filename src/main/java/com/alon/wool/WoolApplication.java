37af428369b48874809b0dae76a67172b10fc0e8
Alon Talmor <alontalmor@gmail.com>
2022-08-20 16:41:50 +0300

    Initial commit

---
+++ src/main/java/com/alon/wool/WoolApplication.java
@@ -0,0 +1,13 @@
+package com.alon.wool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alon/wool/WoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alon.wool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
